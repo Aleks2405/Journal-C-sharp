55fc9e0ac6b1fe2da1b06c813eb7210ff0a7dbc4
Aleksandr <shura.gorodnichev@mail.ru>
2022-02-03 00:02:14 +0300

    Добавьте файлы проекта.

---
+++ Журнал/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace one
+{
+    class Journal
+    {
+        private int quantity;
+
+        public bool oper;
+
+        public Journal(int quantity) { this.quantity = quantity; }
+
+        public static int operator +(Journal a, int x)
+        {
+            return a.quantity + x;
+        }
+        public static int operator - (Journal a, int x)
+        {
+            return a.quantity - x;
+        }
+      
+        
+    }
+}
